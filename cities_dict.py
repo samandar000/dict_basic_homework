6dc59e21696f9c0ca59f10491fd8c76b5f989dd6
Samandar000 <samandarnfh.2020@gmail.com>
2022-09-24 19:33:20 +0500

    01

--- cities_dict.py
+++ cities_dict.py
@@ -6,6 +6,6 @@ def cities_dict(cities:list):
     Returns:
         dict: dictionary with keys ordered by city name
     """
-    cities = { 'a':1,'b':2}
-    return cities['a']
+    cities = {0: 'Samarkand', 1: 'Tashkent', 2: 'Moscow'}
+    return cities
 
